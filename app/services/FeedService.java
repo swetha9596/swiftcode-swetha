b955f0d7f48721ae711ad8a3407955e67eb1a1af
Live session user <shwethasureshabs@gmail.com>
2017-09-16 10:34:14 +0000

    comleted connecting

--- app/services/FeedService.java
+++ app/services/FeedService.java
@@ -14,7 +14,8 @@ public class FeedService {
     public FeedResponse getFeedResponse(String keyword) throws ExecutionException, InterruptedException {
        FeedResponse feedResponseObject = new FeedResponse();
        try{
-           WSRequest feedRequest = WS.url("https://news.google.com/news");            CompletionStage<WSResponse> responsePromise = feedRequest
+           WSRequest feedRequest = WS.url("https://news.google.com/news");            
+           CompletionStage<WSResponse> responsePromise = feedRequest
                    .setQueryParameter("q", keyword)
                    .setQueryParameter("output", "rss")
                    .get();            Document feedResponse = responsePromise.thenApply(WSResponse::asXml)
