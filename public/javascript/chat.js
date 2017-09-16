44294f2f97ff91d7ab83c9ad7bacf8b8fee163f1
Live session user <shwethasureshabs@gmail.com>
2017-09-16 10:28:55 +0000

    Connecting ws to backend

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -1,40 +1,22 @@
 var app = angular.module('chatApp',['ngMaterial']);
 
 app.controller('chatController',function($scope, $sce){
-    $scope.messages = [
-    {
-      sender:"BOT",
-      text:"what can I do for you?",
-      time:"1:12 PM"
-    },
-    {
-      sender:"user",
-      text:"hi2",
-      time:"1:13 PM"
-    },
-    {
-      sender:"BOT",
-      text:"hi3",
-      time:"1:14 PM"
-    },
-    {
-      sender:"user",
-      text:"hi4",
-      time:"1:15 PM"
-    }
-    ];
+
+    $scope.messages = [];
     $scope.sendMessage = function(){
         exampleSocket.send($scope.userMessage);
         $scope.userMessage = "";
     };
-
-    var exampleSocket = new WebSocket("ws://localhost:9000/chatSocket");
-    exampleSocket.onmessage  =   function  (event) {
-           var jsonData = JSON.parse(event.data);
-           jsonData.time = new Date().toLocaleTimeString;
-
-           console.log(jsonData);
-       };
-
-    $scope.trust = $sce.trustAsHtml;
+    var  exampleSocket =  new  WebSocket("ws://localhost:9000/chatSocket");
+    exampleSocket.onmessage  =   function  (event) {        
+    var jsonData = JSON.parse(event.data);  
+    jsonData.time = new Date().toLocaleTimeString();        
+    $scope.messages.push(jsonData);        
+    $scope.$apply();        
+    console.log(jsonData);       
+     };
+     $scope.sendMessage = function () {        
+     exampleSocket.send($scope.userMessage);      
+     $scope.userMessage = ""; };
+     $scope.trust = $sce.trustAsHtml;
 });
